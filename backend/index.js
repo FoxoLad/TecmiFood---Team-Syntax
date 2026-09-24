11e48ac05de1ccfb825aede11d84b56e46719fa7
LuisSeg <luis7seg@gmail.com>
2026-09-23 19:16:45 -0500

    Puerto abierto para la API

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 //Middleware
 app.use(cors());
-app.use(express.json()); //Allows parsing JSON bodies
+app.use(express.json());
 
 //Rutas
 app.use("/api/productos", productRoutes);
@@ -30,7 +30,7 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Conectado a MongoDB Atlas");
-    app.listen(PORT, () => {
+    app.listen(PORT, "0.0.0.0", () => {
       console.log(`Servidor corriendo en http://localhost:${PORT}`);
     });
   })
